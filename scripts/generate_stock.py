bb5cb9950aa0d800799fa05654ea2b22ac8e5dcf
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2025-10-16 23:02:29 +0100

    resume the stock table directly from this script

--- scripts/generate_stock.py
+++ scripts/generate_stock.py
@@ -7,29 +7,62 @@ categories = {'Tops': ["T-Shirt", "Blouse", "Shirt", "Sweater", "Tank Top"],
     'Footwear': ["Sneakers", "Boots", "Sandals", "Heels"],
     'Accessories': ["Belt", "Scarf", "Hat", "Bag", "Sunglasses"]}
 
-def generate_initial_stock(num_lines):
+stock_title = 'item_name, category, stock, min_stock\n'
 
-    stock_content = ''
+def create_stock(num_lines):
+
+    table = ''
     for _ in range(num_lines):
         category = random.choice(list(categories.keys()))
         name = random.choice(categories[category])
         if category == 'Tops' or category == 'Bottoms':
             stock = random.randint(10, 20)
-            min_stock = 5
         elif category == 'Outerwear' or category == 'Dresses':
             stock = random.randint(5, 10)
-            min_stock = 3
         elif category == 'Footwear' or category == 'Accessories':
             stock = random.randint(8, 15)
-            min_stock = 2
-        stock_content += f'{name}, {category}, {stock}, {min_stock}\n'
+        table += f'{name}, {category}, {stock}\n'
 
-    stock_title = 'item_name, category, stock, min_stock\n'
+    return table
+
+def resume_stock(table):
+
+    line_stock = table.split('\n')
+    quant_list_stock = []
+    for i in line_stock[:-1]:
+        name = i.split(', ')[0]
+        quant = int(i.split(', ')[2])
+        quant_list_stock.append({name: quant})
+
+    total_stock = {}
+    for i in quant_list_stock: 
+        for n, q in i.items():
+            if n in total_stock:
+                total_stock[n] += q
+            elif n not in total_stock:
+                total_stock[n] = q
+    
+    stock_content = ''
+    for n, q in total_stock.items():
+        for cat, l in categories.items():
+            if n in l:
+                category = cat
+                break
+        if category == 'Tops' or category == 'Bottoms':
+            min_stock = 5
+        elif category == 'Outerwear' or category == 'Dresses':
+            min_stock = 3
+        elif category == 'Footwear' or category == 'Accessories':
+            min_stock = 2
+
+        stock_content += f'{n}, {category}, {q}, {min_stock}\n'
+
     table = stock_title + stock_content
     return table
 
 if __name__ == '__main__':
-    stock_table = generate_initial_stock(100)
+    raw_table = create_stock(100)
+    stock_table = resume_stock(raw_table)
     print(stock_table)
 
     file = open('data/stock.csv', 'w')
