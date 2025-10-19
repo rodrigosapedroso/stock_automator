917cb52acec9ba9d711f05152c4e77f214884ae2
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2025-10-19 19:14:10 +0100

    added a return value for the show_stock function. Deleted open stock and sales csv variables as those tables are called in main.py variables

--- scripts/restock_order.py
+++ scripts/restock_order.py
@@ -1,10 +1,3 @@
-file_stock = open('data/stock.csv', 'r')
-init_stock = file_stock.read()
-file_stock.close()
-file_sales = open('data/sales.csv', 'r')
-sales = file_sales.read()
-file_sales.close()
-
 from generate_stock import categories
 
 def new_stock(stock, sales):
@@ -14,7 +7,7 @@ def new_stock(stock, sales):
     line_stock = stock.split('\n')
     stock_dict = {}
     for i in line_stock[1:-1]:
-        stock_name = i.split(', ')[0]
+        stock_name = i.split(', ')[0].strip()
         stock_quant = int(i.split(', ')[2])
         stock_min = int(i.split(', ')[3])
         stock_dict[stock_name] = [stock_quant, stock_min]
@@ -22,49 +15,65 @@ def new_stock(stock, sales):
     line_sales = sales.split('\n')
     sales_dict = {}
     for i in line_sales[1:-1]:
-        sales_name = i.split(', ')[0]
+        sales_name = i.split(', ')[0].strip()
         sales_quant = int(i.split(', ')[2])
         sales_dict[sales_name] = sales_quant
 
     content = ''
     alert = False
     for nstock, qstock in stock_dict.items():
-        for nsales, qsales in sales_dict.items():
-            if nstock == nsales:
-                for cat, name in categories.items():
+        qsales = sales_dict.get(nstock.strip(), 0)
+        stock = qstock[0]
+        min_stock = qstock[1]
+        available = stock - qsales
+        if available <= min_stock:
+            restock = 2 * min_stock - available
+            for cat, name in categories.items():
                     if nstock in name:
                         category = cat
                         break
-                stock = qstock[0]
-                min_stock = qstock[1]
-                available = stock - qsales
-                if available <= min_stock:
-                    restock = 2*min_stock - available
-                    content += f'{nstock}, {category}, {restock}\n'
-                    alert = True
+            content += f'{nstock}, {category}, {restock}\n'
+            alert = True
    
     table = title + content
-    return table, alert
+    return table, alert, stock_dict, sales_dict
 
-def show_restock():
+def warn_min(stock_dict, sales_dict):
 
-    restock, alert = new_stock(init_stock, sales)
+    content = []
+    alert = False
+    for nstock, qstock in stock_dict.items():
+        qsales = sales_dict.get(nstock.strip(), 0)
+        stock = qstock[0]
+        min_stock = qstock[1]
+        available = stock - qsales
+        if min_stock < available < (1.5*min_stock):
+            content.append(nstock)
+            alert = True
 
-    file_restock = open('data/restock.csv', 'w')
-    file_restock.write(restock)
-    file_restock.close()
+    content = ', '.join(content) 
+    return content, alert
 
-    if alert:
+def show_restock(init_stock, sales):
+
+    restock, alert2, stock_dict, sales_dict = new_stock(init_stock, sales)
+    items, alert1 = warn_min(stock_dict, sales_dict)
+
+    if alert1:
+        print(f'ATTENTION! Stock is approaching minimum for the following item(s): {items}\n')
+
+    if alert2:
         print('ALERT: New inventory to be restored!\n')
     else:
         print('No stock needed this week.\n')
 
     print(restock)
 
+    file_restock = open('data/restock.csv', 'w')
+    file_restock.write(restock)
+    file_restock.close()
+
+    return restock
+
 if __name__ == '__main__':
-    show_restock()
-
-
-    
-    
-    
+    show_restock(init_stock, sales)
