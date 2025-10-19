095bf2fcf9891c543d5e7dc152f96ec06ff1a20d
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2025-10-19 19:14:40 +0100

    added variables stock and sales that save the data first before returning the restock table

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,6 @@ from generate_stock import show_stock
 from generate_sales import show_sales
 from restock_order import show_restock
 
-show_stock(100)
-show_sales(100)
-show_restock()
+stock = show_stock(100) #input = number of initial stock random lines (example: 100)
+sales = show_sales(100) #input = number of initial sales random lines (example: 100)
+show_restock(stock, sales)
